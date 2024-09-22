dbae9415d366c5115feb1887ac390fcb29dc55f5
Stephen Mwangi <mukuastephen08@gmail.com>
2024-09-22 22:37:23 +0300

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+sum=number1+number2
+x=number1-number2
+y=number1*number2
+print("Addition of 10 and 5 is ", sum)
+print("Subtraction of 10 and 5 is ", x)
+print("mutiplication of 10 and 5 is ",y)
