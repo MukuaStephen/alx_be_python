09c1f4343f4d9a1941522e25a8d351947106f457
Stephen Mukua <mukuastephen08@gmail.com>
2024-09-30 00:44:10 +0300

    changes made

--- control-flow/mutiplication_table.py
+++ control-flow/mutiplication_table.py
@@ -1,4 +1,5 @@
 number = input("Enter a number to see its mutiplication table: ")
 number = int(number)
 for i in range(1, 11):
-    print(f"{number} * {i} = {number * i}")
+    product = number * i
+    print(f"{number} * {i} = {product}")
