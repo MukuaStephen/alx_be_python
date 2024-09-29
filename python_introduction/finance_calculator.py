d56e294238f69f307ae521668bef607789a2b724
Stephen Mukua <mukuastephen08@gmail.com>
2024-09-30 01:44:23 +0300

    changing errors

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,7 @@
-income = input("Enter your monthly income:")
-income = int(income)
-expenses = input("Enter your total monthly expenses:")
-expenses = int(expenses)
-month_saving = income - expenses
-print("Your monthly savings are $", month_saving)
-annual_saving = month_saving * 12 + (month_saving * 12 * 0.05)
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+print("Your monthly savings are $", monthly_savings)
+annual_saving = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 annual_saving = int(annual_saving)
 print("Projected savings after one year, with interest, is: $", annual_saving)
